b1e6b9d042b5658964c23b1f043a44adf2dec213
razibmahmud50 <razibmahmud50@gmail.com>
2019-11-26 19:42:37 +0600

    vue state management

---
+++ src/components/stats.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="stats">
+    <h1>another components here</h1>
+    <p>you have {{counterLinks}} link</p>
+    <button v-on:click="removeAllLinks">Remove all links</button>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations, mapActions } from 'vuex'
+export default {
+  name: 'stats',
+  computed: {
+    ...mapGetters([
+      'counterLinks'
+    ])
+  },
+  methods: {
+    ...mapMutations(['REMOVE_ALL']),
+    ...mapActions(['removeAll']),
+    removeAllLinks() {
+      this.removeAll().then(() => {
+        this.msg = 'They have been removed'
+      });
+    }
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+button {
+    padding: 10px;
+    margin-top: 30px;
+    width: 100%;
+    background: none;
+    border: 1px solid lightgray;
+    outline: 0;
+    cursor: pointer;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,42 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    title: "hello vuex",
+    links: [
+      'www.google.com',
+      'www.youtube.com',
+      'www.facebook.com',
+    ]
   },
+  getters: {
+    counterLinks: state=>{
+       return state.links.length
+    }
+  }
+  ,
   mutations: {
+    ADDLINK: (state, link)=>{
+      state.links.push(link)
+    },
+    REMOVE_LINK: (state, link)=>{
+      state.links.splice(link, 1)
+    },
+    REMOVE_ALL: (state)=>{
+      state.links=[]
+    }
   },
   actions: {
+    removeLink: (context, link)=>{
+      context.commit('REMOVE_LINK', link)
+    },
+    removeAll ({commit}) {                       // Add this
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          commit('REMOVE_ALL')
+          resolve()
+        }, 1500)
+      })
+    }
   },
   modules: {
   }
